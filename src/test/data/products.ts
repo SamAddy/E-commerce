d69177d06ae6a30cfd1376f8b458b62eaad72681
SamAddy <addisonsamuel70@yahoo.com>
2023-05-16 21:41:52 +0300

    Add dummy product for testing

---
+++ src/test/data/products.ts
@@ -0,0 +1,153 @@
+import { Product } from "../../type/Product";
+
+const product1: Product = {
+    id: 770,
+    title: "Product 1",
+    price: 50,
+    description: "Product 1 description",
+    category: {
+        id: 1,
+        name: "Category 1",
+        image: "https://placeimg.com/640/480/any?r=0.591926261873231"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product2: Product = {
+    id: 771,
+    title: "Product 2",
+    price: 150,
+    description: "Product 2 description",
+    category: {
+        id: 2,
+        name: "Category 2",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product3: Product = {
+    id: 772,
+    title: "Product 3",
+    price: 70,
+    description: "Quality shoes for vibes",
+    category: {
+        id: 2,
+        name: "Category 2",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product4: Product = {
+    id: 773,
+    title: "Product 4",
+    price: 149,
+    description: "Product 4 description",
+    category: {
+        id: 3,
+        name: "Category 3",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product5: Product = {
+    id: 774,
+    title: "Product 5",
+    price: 149,
+    description: "Product 5 description",
+    category: {
+        id: 3,
+        name: "Category 3",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product6: Product = {
+    id: 775,
+    title: "Product 6",
+    price: 574,
+    description: "Product 6 description",
+    category: {
+        id: 1,
+        name: "Category 1",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product7: Product = {
+    id: 776,
+    title: "Product 7",
+    price: 149,
+    description: "Product 7 description",
+    category: {
+        id: 3,
+        name: "Category 3",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product8: Product = {
+    id: 777,
+    title: "Rock runner",
+    price: 149,
+    description: "Run every where",
+    category: {
+        id: 2,
+        name: "Sport",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product9: Product = {
+    id: 778,
+    title: "Jordans",
+    price: 149,
+    description: "Vibe on a new frequency",
+    category: {
+        id: 3,
+        name: "Fashinon",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+const product10: Product = {
+    id: 779,
+    title: "Casual Hoody",
+    price: 149,
+    description: "Normal vibes",
+    category: {
+        id: 1,
+        name: "Hoodies",
+        image: "https://placeimg.com/640/480/any?r=54370078"
+    },
+    images: [
+        "https://placeimg.com/640/480/any?r=0.9178516507833767"
+    ]
+}
+
+export { product1, product2, product3, product4, product5}
