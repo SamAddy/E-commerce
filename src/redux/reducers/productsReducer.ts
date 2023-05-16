bf3c18466e6c4b3800e03fb839aa49c9ccf592f0
SamAddy <addisonsamuel70@yahoo.com>
2023-05-16 12:13:02 +0300

    Modify reducers to update on endpoint' '

--- src/redux/reducers/productsReducer.ts
+++ src/redux/reducers/productsReducer.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 import { Product } from "../../type/Product";
 import { ErrorValidation } from "../../type/Error";
 import { ProductUpdate } from "../../type/ProductUpdate";
+import { create } from "domain";
 
 const initialState : Product[] = []
 
@@ -12,6 +13,44 @@ export const fetchAllProducts = createAsyncThunk(
     async () => {
         try {
             const response = await axios.get<Product[]>("https://api.escuelajs.co/api/v1/products")
+            return response.data
+        }
+        catch(error) {
+            if (axios.isAxiosError<ErrorValidation, Record<string, string[]>>(error)) {
+                console.log(error.status)
+                console.log(error.response)
+            }
+            else {
+                console.log(error)
+            }
+        }
+    }
+)
+
+export const addNewProduct = createAsyncThunk(
+    "createProduct",
+    async (product) => {
+        try{
+            const response = await axios.post("https://api.escuelajs.co/api/v1/products/", product)
+            return response.data
+        }
+        catch(error) {
+            if (axios.isAxiosError<ErrorValidation, Record<string, string[]>>(error)) {
+                console.log(error.status)
+                console.log(error.response)
+            }
+            else {
+                console.log(error)
+            }
+        }
+    }
+)
+
+export const updateExistingProduct = createAsyncThunk(
+    "updateProduct",
+    async (product: ProductUpdate) => {
+        try {
+            const response = await axios.put<Product[]>(`https://api.escuelajs.co/api/v1/products/${product.id}`, product.update)
             return response.data
         }
         catch(error) {
@@ -37,8 +76,6 @@ const productsSlice = createSlice({
             return action.payload
         },
         updateProduct: (state, action: PayloadAction<ProductUpdate>) => {
-            // The checks whether the user is an admin only will be made at where
-            // the updateAProduct method will be called.
             return state.map(product => {
                 if (product.id === action.payload.id) {
                     return {...product, ...action.payload.update}
@@ -47,9 +84,17 @@ const productsSlice = createSlice({
             })
         },
         deleteProduct: (state, action: PayloadAction<Product>) => {
-            // This feature is also going to be only for admin
-            // checks will be made when this method is been called before its been executed.
             return state.filter(product => product.id !== action.payload.id)
+        }, 
+        sortByCategory: (state) => {
+        },
+        sortByPrice: (state, action: PayloadAction<"asc"|"desc">) => {
+            if (action.payload === "asc") {
+                state.sort((a,b) => a.price - b.price)
+            } 
+            else {
+                state.sort((a,b) => b.price - a.price)
+            }  
         }
     },
     extraReducers: (build) => {
@@ -58,9 +103,24 @@ const productsSlice = createSlice({
                 return action.payload
             }
         })
+        build.addCase(addNewProduct.fulfilled, (state, action) => {
+            state.push(action.payload)
+        })
+        build.addCase(updateExistingProduct.fulfilled, (state, action) => {
+                const updatedIndex = state.findIndex((product) => product.id === action.payload.id)
+                if (updatedIndex !== -1) {
+                    state[updatedIndex] = action.payload
+                }
+        })
     }
 })
 
 const productsReducer = productsSlice.reducer
-export const { createProduct, updateProductReducer, updateProduct, deleteProduct} = productsSlice.actions
+export const { 
+    createProduct, 
+    updateProductReducer, 
+    updateProduct, 
+    deleteProduct,
+    sortByPrice
+} = productsSlice.actions
 export default productsReducer
