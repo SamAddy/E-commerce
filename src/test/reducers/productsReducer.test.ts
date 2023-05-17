1c5d552a216dd3f50620ded55f7c363942e1289c
SamAddy <addisonsamuel70@yahoo.com>
2023-05-17 21:43:10 +0300

    Add sample test for product reducer

--- src/test/reducers/productsReducer.test.ts
+++ src/test/reducers/productsReducer.test.ts
@@ -1,8 +1,7 @@
-import productsReducer, { addNewProduct, fetchAllProducts, updateExistingProduct } from "../../redux/reducers/productsReducer"
+import productsReducer, { addNewProduct, cleanUpProductReducer, fetchAllProducts, sortProductByPrice, updateExistingProduct } from "../../redux/reducers/productsReducer"
+
 import store from "../../redux/store"
-import { CreatProduct } from "../../type/CreateProduct"
-import { Product } from "../../type/Product"
-import { product1, product2, product3, product4, product5 } from "../data/products"
+import { invalidProduct, newProduct, newProduct2, product1, product2, product3, product4, product5, updateProduct } from "../data/products"
 import productServer from "../mock/productServer"
 
 beforeAll(() => {
@@ -14,8 +13,9 @@ afterAll(() => {
 })
 
 beforeEach(() => {
-    store.dispatch({ type: "RESET"})
+    store.dispatch(cleanUpProductReducer)
 })
+
 describe("Testing productsReducer", () => {
     test("Check initialState", () => {
         const state = productsReducer(undefined, { type: "unknown" })
@@ -32,22 +32,38 @@ describe("Testing productsReducer", () => {
         expect(store.getState().productsReducer.loading).toBeFalsy()
         expect(store.getState().productsReducer.error).toBeFalsy()
     })
-    test("Check should update existing product", async () => {
-        const product = {
-            id: 781,
-            update: {
-                title: "Updated Product",
-                price: 5.99,
-                description: "Update description",
-                category: [
-                    {
-                        id: 1,
-                        name: "Category 1",
-                        image: ["https://testimage.com/updated.png"]
-                    }
+    test("Check should add new product", async () => {
+        await store.dispatch(addNewProduct(newProduct))
+        expect(store.getState().productsReducer.products.length).toBe(6)
+        expect(store.getState().productsReducer.products[5].title).toBe("New Product 1")
+    })
+    test("Check should check if invalid product has been created", async () => {
+        await store.dispatch(addNewProduct(invalidProduct))
+        expect(store.getState().productsReducer.products.length).toBe(6)
+        expect(store.getState().productsReducer.error.length).toBeGreaterThan(0)
+        expect(store.getState().productsReducer.error).toBe(JSON.stringify(
+            {
+                statusCode: 400,
+                message: [
+                    "Price must be greater than 0",
+                    "Images must contain at least 1 image.",
+                    "Category does not exist"
                 ],
-                images: ["https://testimage.com/updated2.png"]
+                error: "Bad Request"
             }
-        }
+        ))
+    })
+    test("Check should update existing product", async () => {
+        await store.dispatch(updateExistingProduct(updateProduct))
+        const state = store.getState().productsReducer.products
+        expect(state[0].title).toBe("Updated Product")
+    })
+    test("Check should sort products by price in ascending order", ()  => {
+        store.dispatch(sortProductByPrice("asc"))
+        expect(store.getState().productsReducer.products).toEqual([product1, product2, product3, product4, product5])
+    })
+    test("Check should sort products by price in descending order", ()  => {
+        store.dispatch(sortProductByPrice("desc"))
+        expect(store.getState().productsReducer.products).toEqual([product1, product2, product3, product4, product5])
     })
 })
