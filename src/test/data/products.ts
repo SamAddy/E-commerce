2e4738ab55567f76a0641e3f42162d16be3d8ea0
SamAddy <addisonsamuel70@yahoo.com>
2023-05-17 21:41:46 +0300

    Update dummy products

--- src/test/data/products.ts
+++ src/test/data/products.ts
@@ -1,15 +1,14 @@
+import { CreateProduct } from "../../type/CreateProduct";
 import { Product } from "../../type/Product";
+import { ProductUpdate } from "../../type/ProductUpdate";
+import { category1, category2, category3 } from "./categories";
 
 const product1: Product = {
     id: 770,
     title: "Product 1",
     price: 50,
     description: "Product 1 description",
-    category: {
-        id: 1,
-        name: "Category 1",
-        image: "https://placeimg.com/640/480/any?r=0.591926261873231"
-    },
+    category: category1,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
@@ -20,11 +19,7 @@ const product2: Product = {
     title: "Product 2",
     price: 150,
     description: "Product 2 description",
-    category: {
-        id: 2,
-        name: "Category 2",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
+    category: category2,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
@@ -35,11 +30,7 @@ const product3: Product = {
     title: "Product 3",
     price: 70,
     description: "Quality shoes for vibes",
-    category: {
-        id: 2,
-        name: "Category 2",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
+    category: category3,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
@@ -50,11 +41,7 @@ const product4: Product = {
     title: "Product 4",
     price: 149,
     description: "Product 4 description",
-    category: {
-        id: 3,
-        name: "Category 3",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
+    category: category1,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
@@ -65,11 +52,7 @@ const product5: Product = {
     title: "Product 5",
     price: 149,
     description: "Product 5 description",
-    category: {
-        id: 3,
-        name: "Category 3",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
+    category: category2,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
@@ -80,74 +63,43 @@ const product6: Product = {
     title: "Product 6",
     price: 574,
     description: "Product 6 description",
-    category: {
-        id: 1,
-        name: "Category 1",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
+    category: category3,
     images: [
         "https://placeimg.com/640/480/any?r=0.9178516507833767"
     ]
 }
 
-const product7: Product = {
-    id: 776,
-    title: "Product 7",
-    price: 149,
-    description: "Product 7 description",
-    category: {
-        id: 3,
-        name: "Category 3",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
-    images: [
-        "https://placeimg.com/640/480/any?r=0.9178516507833767"
-    ]
+const newProduct: CreateProduct = {
+    title: "New Product 1",
+    price: 229,
+    description: "New Product here",
+    images: [""],
+    categoryId: 2
 }
 
-const product8: Product = {
-    id: 777,
-    title: "Rock runner",
-    price: 149,
-    description: "Run every where",
-    category: {
-        id: 2,
-        name: "Sport",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
-    images: [
-        "https://placeimg.com/640/480/any?r=0.9178516507833767"
-    ]
+const newProduct2: CreateProduct = {
+    title: "New Product 2",
+    price: 9.99,
+    description: "New Product two here",
+    images: ["https://placeimg.com/640/480/any?r=0.9178516507833767"],
+    categoryId: 3
 }
 
-const product9: Product = {
-    id: 778,
-    title: "Jordans",
-    price: 149,
-    description: "Vibe on a new frequency",
-    category: {
-        id: 3,
-        name: "Fashinon",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
-    images: [
-        "https://placeimg.com/640/480/any?r=0.9178516507833767"
-    ]
+const invalidProduct: CreateProduct = {
+    title: "Invalid product 1",
+    price: 0,
+    description: "new product",
+    images: [],
+    categoryId: 4
 }
 
-const product10: Product = {
-    id: 779,
-    title: "Casual Hoody",
-    price: 149,
-    description: "Normal vibes",
-    category: {
-        id: 1,
-        name: "Hoodies",
-        image: "https://placeimg.com/640/480/any?r=54370078"
-    },
-    images: [
-        "https://placeimg.com/640/480/any?r=0.9178516507833767"
-    ]
+const updateProduct: ProductUpdate = {
+    id: 770,
+    update: {
+        title: "Updated Product",
+        price: 0
+    }
 }
 
-export { product1, product2, product3, product4, product5}
+export const products = [product1, product2, product3, product4, product5, product6]
+export { product1, product2, product3, product4, product5, product6, newProduct, newProduct2, invalidProduct, updateProduct }
