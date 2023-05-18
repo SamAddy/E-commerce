3f2d631cb97cb12396db6daf1e42e12ddfb13e0f
SamAddy <addisonsamuel70@yahoo.com>
2023-05-18 22:00:37 +0300

    Add new user interface

--- src/type/User.ts
+++ src/type/User.ts
@@ -1,8 +1,32 @@
 export interface User {
-    id: string
+    id: number
     email: string
     password: string
     name: string
     role: "admin" | "customer"
+    avatar: string
+}
+
+export interface UserState {
+    users: User[],
+    currentUser?: User,
+    loading: boolean,
+    error: string
+}
+
+export interface UserCredentials {
+    email: string
+    password: string
+}
+
+export interface UserUpdate {
+    id: number
+    update: Omit<User, "id">
+}
+
+export interface NewUser {
+    name: string
+    email: string
+    password: string
     avatar: string
 }
