b387316d4281dc8b9590cdb3f3c3cb2d44130709
SamAddy <addisonsamuel70@yahoo.com>
2023-05-25 22:10:30 +0300

    Update registration form to include avatar link

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,72 +1,134 @@
-import React, { useEffect, useState } from 'react'
-import { Box, TextField } from '@mui/material'
-import { useForm } from "react-hook-form"
+import React from 'react'
+import { Avatar, Box, Button, Container, CssBaseline, Grid, TextField, ThemeProvider, Typography, createTheme } from '@mui/material'
 
-import CustomBtn from '../styles/component/CustomBtn'
-import Header from './Header'
-import useCustomSelector from '../hooks/useCustomSelector'
-import { useFormAction } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { registerUser } from '../redux/reducers/usersReducer'
+import { useDispatch } from 'react-redux'
+
+const Copyright = (props: any) => {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" to="/">
+                Shop Goodies
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
+
+const defaultTheme = createTheme()
 
 const RegisterPage = () => {
-    const [customError, setCustomError] = useState(null)
-    const { users, loading, error } = useCustomSelector((state) => state.usersReducer)
-    const { register, handleSubmit } = useForm()
-
-    // const handleSubmit = (userData) => {
-    //     if (userData.)
-    // }
+    const dispatch = useDispatch()
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+        const name = data.get("name") as string
+        const email = data.get("email") as string
+        const password = data.get("password") as string
+        const avatar = data.get("avatar") as string
+        try {
+            dispatch(registerUser({ name, email, password, avatar }))
+        }
+        catch (error) {
+            console.log("Registration failed: ", error)
+        }
+    }
     return (
-        <div>
-            <Header />
-            <main>
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
                 <Box
-                    component="form"
                     sx={{
-                        width: 500,
-                        maxWidth: "100%",
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
                     }}
-                    
                 >
-                    <TextField
-                        fullWidth
-                        required
-                        label="Name"
-                        id="fullWidth"
-                        margin="normal"
-                        {...register("name")}
-                    />
-                    <TextField
-                        fullWidth
-                        required
-                        label="E-mail"
-                        id="fullWidth"
-                        margin="normal"
-                        className='input'
-                        {...register("email")}
-                    // value={user?.name || ''}
-                    />
-                    <TextField
-                        fullWidth
-                        required
-                        label="Password"
-                        type="password"
-                        id="fullWidth"
-                        margin="normal"
-                        {...register("password")}
-                    // value={user?.email || ''}
-                    />
-                    <TextField
-                        fullWidth
-                        required
-                        label="Password"
-                        id="fullWidth"
-                        margin="normal"
-                    // value={user?.email || ''}
-                    />
-                    <CustomBtn type="submit">Register</CustomBtn>
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        {/* <LockOutlinedIcon /> */}
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="name"
+                                    required
+                                    fullWidth
+                                    id="name"
+                                    label="Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="confirm-password"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="password"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="avatar"
+                                    label="Avatar URL"
+                                    type="url"
+                                    id="avatar"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <Link to="/login" color="inherit">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Box>
-            </main>
-        </div>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
     )
 }
 
