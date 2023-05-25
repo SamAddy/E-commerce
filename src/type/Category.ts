027e3a00f5781ffb77953f08b3b90bf98990a942
SamAddy <addisonsamuel70@yahoo.com>
2023-05-25 22:16:06 +0300

    Add category interface

--- src/type/Category.ts
+++ src/type/Category.ts
@@ -1,5 +1,14 @@
+import { Product } from "./Product"
+
 export interface Category {
     id: number
     name: string
     image: string
+}
+
+export interface CategoryState {
+    categories: Category[]
+    loading: boolean
+    error: string
+    categoryProducts?: Product[]
 }
