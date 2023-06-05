4a19ba6f8bf0d02d8b4fe10f1ce49c0f881d7ea8
SamAddy <addisonsamuel70@yahoo.com>
2023-06-05 18:46:18 +0300

    Update login page to use login component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,69 +1,13 @@
-import React, { useEffect } from 'react'
-import { Box, TextField, Typography } from '@mui/material'
-
-import Header from '../component/Header'
-import CustomBtn from '../styles/component/CustomBtn'
-import useCustomSelector from '../hooks/useCustomSelector'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router'
-import { useForm } from 'react-hook-form'
-import { login } from '../redux/reducers/usersReducer'
-import store from '../test/shared/store'
-import { Link } from 'react-router-dom'
+import Header from "../component/Header";
+import Login from "../component/Login";
 
 const LoginPage = () => {
-    const { users, loading, error, currentUser } = useCustomSelector((state) => state.usersReducer)
-    const isLoggedIn = useCustomSelector((state) => state.usersReducer.isLoggedIn)
-    const { register, handleSubmit } = useForm<{ email: string, password: string }>()
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const handleLogin = (data: { email: string, password: string }) => {
-        const { email, password } = data
-        store.dispatch(login({ email: email, password: password })).then((user) => {
-            if (user.meta.requestId == "fulfilled") {
-                navigate("/")
-            }
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
-    return (
-        <div>
-            <Header />
-            <Box className="form">
-                <Box
-                    component="form"
-                    onSubmit={handleSubmit(handleLogin)}
-                >
-                    <Typography component="h4" variant="h4">
-                        Login
-                    </Typography>
-                    <TextField
-                        fullWidth
-                        {...register('email', { required: true })}
-                        label="E-mail"
-                        id="fullWidth"
-                        margin="normal"
-                        className='input'
-                    />
-                    <TextField
-                        fullWidth
-                        {...register('password', { required: true })}
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        id="fullWidth"
-                        margin="normal"
-                    />
-                    <CustomBtn type="submit" disabled={loading} >Log In</CustomBtn>
-                    <Typography variant="body1" component="body">
-                        Don't have an account?
-                        <Link to="/register">Register</Link>
-                    </Typography>
-                </Box>
-            </Box>
-        </div>
-    )
+  return (
+    <div>
+        <Header />
+        <Login />
+    </div>
+  )
 }
 
-export default LoginPage
+export default LoginPage;
